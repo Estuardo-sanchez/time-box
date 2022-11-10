7101a62beb12c8f3b45de509803e9a40d1accaf2
estuardo sanchez <estuardo_101@hotmail.com>
2022-11-09 21:48:07 -0500

    add css styling

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,36 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
   const [input, setInput] = useState('');
 
-  function handleChange(e) {
-    setInput(e.target.value);
-  }
+  const inputRef = useRef(null);
 
-  function handleSubmit(e) {
+  useEffect(() => {
+    inputRef.current.focus()
+  })
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
     e.preventDefault();
 
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
-      Text: input
-    });
-
-    setInput('');
-  }
+      text: input
+    })
+    setInput('')
+  };
 
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
       <input
         type="text"
-        placeholder='Add a todo'
+        placeholder="Add a todo"
         value={input}
-        name="tex"
+        name="text"
         className='todo-input'
         onChange={handleChange}
+        ref={inputRef}
       />
-      <button className='todo-button'> Add todo</button>
+      <button className="todo-button">Add Todo</button>
     </form>
-  )
+  );
 }
 
 export default TodoForm
